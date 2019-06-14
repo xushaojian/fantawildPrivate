90680d451ba8dac174c10c28908feec29364c196
xushaojian <32660482+xushaojian@users.noreply.github.com>
2019-06-14 15:30:19 +0800

    12

--- todos/src/components/Footer.js
+++ todos/src/components/Footer.js
@@ -4,15 +4,15 @@ import FilterLink from '../containers/FilterLink';
 
 const Footer = () => (
   <div>
-    <span>显ok示 : </span>
+    <span>显示 : </span>
     <FilterLink filter={VisibilityFilters.SHOW_ALL}>
       所有
     </FilterLink>
     <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
-      待ok办123
+      待办1
     </FilterLink>
     <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
-      已完成o98k
+      已完成ok
     </FilterLink>
   </div>
 )
